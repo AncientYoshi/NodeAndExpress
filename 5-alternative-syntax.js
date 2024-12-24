d42efcd97dabaf7857dbada0d8a4fbbbdb67ba0a
AncientYoshi <paiswanpyae2002@gmail.com>
2024-12-24 16:29:54 +0630

    added alternative syntax to know

---
+++ 5-alternative-syntax.js
@@ -0,0 +1,5 @@
+module.exports.array = ["a", "b", "x", "y", "z"];
+const person = {
+  name: "John",
+};
+module.exports.singlePerson = person;
--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const names = require("./3-names");
 const sayHi = require("./4-utils");
+const data = require("./5-alternative-syntax");
+console.log(data);
 
 sayHi(names.James);
 sayHi(names.peter);
